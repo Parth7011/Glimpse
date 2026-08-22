6da8fe19b71583fc18892de890723e1d935d542e
Parth Mittal <parthmittal701@gmail.com>
2026-08-22 22:55:29 +0530

    fix: remove GuestDashboardLayout export to fix Vercel build

--- frontend/src/components/layout/index.js
+++ frontend/src/components/layout/index.js
@@ -1,5 +1,4 @@
 export { Navbar } from './Navbar';
 export { Footer } from './Footer';
 export { DashboardLayout } from './DashboardLayout';
-export { GuestDashboardLayout } from './GuestDashboardLayout';
 export { GuestLayout } from './GuestLayout';
